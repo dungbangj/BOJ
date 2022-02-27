ddaff09b60a877458f2ab8a675951ec41fbbc8ec
dungbangj <yoyob1223@gmail.com>
2022-02-27 22:18:25 +0900

    13702 BinarySearch

---
+++ main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//백준 13702번: 이상한 술집
+public class main {
+     public static void main(String[] args) throws NumberFormatException, IOException {
+          //key point: 제일 큰 막걸리 용량을 몇으로 나눌지
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          int N = Integer.parseInt(st.nextToken());
+          int K = Integer.parseInt(st.nextToken());
+
+          //배열 생성
+          long[] arr = new long[N];
+          for(int i=0; i<N; i++) {
+               arr[i] = Integer.parseInt(br.readLine());
+          }
+          insertSort(arr);//내림차순 정렬
+
+          int num=0;	//마실 수 있는 총 인원
+          long mak=0;	//나눈 막걸리 용량
+          int i=1; 	//제일 큰 막걸리 용량을 나눌 인원
+
+          while(num<K) {
+               System.out.println("i = " + i + "-------------------------------------------");
+               num=i;
+               mak=arr[0]/i; //arr[0] 가장 큰 수
+               System.out.println("arr[0] = " + arr[0] + ", i = " + i + ", mak = " + mak);
+               for(int j=1; j<N; j++) {
+                    num += arr[j]/mak;
+                    System.out.println("arr[j] = " + arr[j] + ", mak = " + mak + ", num =" + num);
+               }
+               i++;
+          }
+          System.out.println(mak);
+     }
+
+     //내림차순 정렬
+     private static void insertSort(long[] array) {
+          long temp = 0;
+          int j = 0;
+          for(int i=1; i<array.length; i++){
+               temp = array[i];
+               j = i;
+               while(j > 0 && temp > array[j-1]){
+                    array[j] = array[j-1];
+                    j--;
+               }
+               array[j] = temp;
+          }
+     }
+}
