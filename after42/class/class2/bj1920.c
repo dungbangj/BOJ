ea05889b069ecb62e563880981c8d1bab5bab6ea
dungbangj <yoyob1223@gmail.com>
2022-06-26 10:36:40 +0900

    20220626_class2

--- after42/class/class2/bj1920.c
+++ after42/class/class2/bj1920.c
@@ -3,7 +3,15 @@
 
 int compare(const void *a, const void *b)
 {
-    
+    int num1 = *(int *)a;
+    int num2 = *(int *)b;
+
+    if (num1 < num2)
+        return -1;
+    else if (num1 > num2)
+        return 1;
+
+    return 0;
 }
 
 void BinarySearch(int *A, int num, int N)
