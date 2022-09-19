938b5ce50797fb8ea34ea2d951d0b808dffe8c36
dungbangj <yoyob1223@gmail.com>
2022-09-19 13:47:35 +0900

    4 backtraking solved

---
+++ steps/backtracking/src/BOJ15651.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15651 {
+
+     static StringBuilder sb = new StringBuilder();
+     static boolean[] visit;
+     static int[] arr;
+
+     public static void main(String[] args) throws IOException {
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          int N = Integer.parseInt(st.nextToken());
+          int M = Integer.parseInt(st.nextToken());
+          visit = new boolean[N];
+          arr = new int[M];
+          dfs(N, M, 0);
+          System.out.println(sb);
+     }
+
+     static void dfs(int N, int M, int depth) {
+
+          if (M == depth) {
+               for (int val : arr) {
+                    sb.append(val).append(' ');
+               }
+               sb.append('\n');
+               return;
+          }
+
+          for (int i = 0; i < N; i++) {
+                    arr[depth] = i + 1;
+                    dfs(N, M, depth + 1);
+          }
+
+     }
+}
