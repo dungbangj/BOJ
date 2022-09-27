b2d419a8721737891a8d524eb12e6593d7a4f232
dungbangj <yoyob1223@gmail.com>
2022-09-27 16:53:57 +0900

    BOJ 11726 good

---
+++ anotherVersion/untitled/src/Silver/BOJ11726.java
@@ -0,0 +1,21 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11726 {
+     public static void main(String[] args) throws IOException {
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          int N = Integer.parseInt(br.readLine());
+          int[] dp = new int[N + 1];
+          for (int i = 0; i <= N; i++) {
+               if (i == 0) dp[0] = 0;
+               else if (i == 1) dp[1] = 1;
+               else if (i == 2) dp[2] = 2;
+               else
+                    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+          }
+          System.out.println(dp[N]);
+     }
+}
