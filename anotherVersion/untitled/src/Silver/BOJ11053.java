18b29922baea0114312540c95630bcc4cded4b52
dungbangj <yoyob1223@gmail.com>
2022-10-10 14:29:24 +0900

    BOJ 11055 fun

---
+++ anotherVersion/untitled/src/Silver/BOJ11053.java
@@ -0,0 +1,32 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11053 {
+     public static void main(String[] args) throws IOException {
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          int N = Integer.parseInt(br.readLine());
+          int[] arr = new int[N];
+          int[] dp = new int[N];
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          for (int i = 0; i < N; i++) {
+               arr[i] = Integer.parseInt(st.nextToken());
+          }
+          dp[0] = 1;
+          int dpMax = 1;
+          for (int i = 1; i < N; i++) {
+               int max = 0;
+               for (int j = 0; j < i; j++) {
+                    if (dp[j] > max && arr[i] > arr[j])
+                         max = dp[j];
+               }
+               dp[i] = max + 1;
+               if (dpMax < dp[i])
+                    dpMax = dp[i];
+          }
+          System.out.println(dpMax);
+     }
+}
---
+++ anotherVersion/untitled/src/Silver/BOJ11055.java
@@ -0,0 +1,35 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11055 {
+     public static void main(String[] args) throws IOException {
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          int N = Integer.parseInt(br.readLine());
+          int[] arr = new int[N];
+          int[] dp = new int[N];
+
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          for (int i = 0; i < N; i++) {
+               arr[i] = Integer.parseInt(st.nextToken());
+          }
+          dp[0] = arr[0];
+          for (int i = 1; i < N; i++) {
+               dp[i] = arr[i];
+               for (int j = 0; j < i; j++) {
+                    if (arr[j] < arr[i]) {
+                         dp[i] = Math.max(dp[j] + arr[i], dp[i]);
+                    }
+               }
+          }
+          int max = -1;
+          for (int i = 0; i < N; i++) {
+               if (dp[i] > max)
+                    max = dp[i];
+          }
+          System.out.println(max);
+     }
+}
