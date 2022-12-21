a7858fb114bf72929a600fcc2b5158632a3d9697
yback1223 <yoyob1223@gmail.com>
2022-12-21 09:08:42 +0900

    Edit 1697

--- python_algorithm/week06/1697.py
+++ python_algorithm/week06/1697.py
@@ -6,14 +6,11 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 visited = [False] * 100001
 
-print("count", "len(queue)", "queue")
-
 def bfs(N):
     count = 0
     queue = [N]
     while len(queue) > 0:
         for _ in range(len(queue)):
-            print(count, len(queue), queue)
             num = queue.pop(0)
             if num == K:
                 return count
