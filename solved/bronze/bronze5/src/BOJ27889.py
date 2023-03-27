531a5dffa756564ee7c3273fc48e90a5888c7aab
yback1223 <yoyob1223@gmail.com>
2023-03-27 19:00:58 +0900

    Add 1 b5

---
+++ solved/bronze/bronze5/src/BOJ27889.py
@@ -0,0 +1,10 @@
+input_str = input()
+
+if input_str == "NLCS":
+	print("North London Collegiate School")
+elif input_str == "BHA":
+	print("Branksome Hall Asia")
+elif input_str == "KIS":
+	print("Korea International School")
+else:
+	print("St. Johnsbury Academy")
