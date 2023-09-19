a704a214880ed6e4c21e12038e517a147ebeb4f3
yback1223 <yoyob1223@gmail.com>
2023-09-19 17:00:09 +0900

    Solve 2 golds - second time

--- solved/gold/BOJ10986.java
+++ solved/gold/BOJ10986.java
@@ -1,53 +1,44 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class BOJ10986 {
-
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) throws IOException {
-		String[] input = br.readLine().split(" ");
-		int N = Integer.parseInt(input[0]);
-		int M = Integer.parseInt(input[1]);
+		int[] inputNM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int N = inputNM[0];
+		int M = inputNM[1];
+
 		long count = 0;
-		long[] sumArr = new long[N + 1];
-		long[] sameModArr = new long[M];
-		input = br.readLine().split(" ");
+		long[] sumPlusArr = new long[N + 1];
+		long[] sameModeArr = new long[M];
+		int[] inputNums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
 		for (int i = 1; i <= N; i++) {
-			fillSumArr(input, M, sumArr, i);
-			if (isDividable(sumArr[i])) {
-				count++;
-			}
-			sameModArr[(int) sumArr[i]]++;
+			/**
+			 * 누적합 배열을 누적합으로 채우지 않고, 바로 M으로 나눈 나머지를 저장해준다. M으로 바로 나눠 떨어진다면 count를 증가시킨다.
+			 */
+			sumPlusArr[i] = (sumPlusArr[i - 1] + inputNums[i - 1]) % M;
+			if (sumPlusArr[i] == 0) count++;
+			/**
+			 * 나머지를 저장하는 배열을 증가시킨다.
+			 */
+			sameModeArr[(int) sumPlusArr[i]]++;
 		}
+//		System.out.println(Arrays.toString(sumPlusArr));
 
 		for (int i = 0; i < M; i++) {
-			if (sameModArr[i] > 1) {
-				count += (sameModArr[i] * (sameModArr[i] - 1) / 2);
-//				count += combination((int) sameModArr[i], 2);
-			}
+			/**
+			 * 3C2 + 2C2 = (3 * 2) / (2 * 1) + (2 * 1) / (2 * 1) = 4
+			 *
+			 * 공식
+			 * sameModeArr[i]C2 = (sameModeArr[i] * (sameModeArr[i] - 1)) / 2
+			 */
+			if (sameModeArr[i] > 1) count += (sameModeArr[i] * (sameModeArr[i] - 1) / 2);
 		}
 		bw.write(count + "\n");
 		bw.flush();
 		bw.close();
-	}
-
-	private static boolean isDividable(long sumArr) {
-		return sumArr == 0;
-	}
-
-	private static void fillSumArr(String[] input, int M, long[] sumArr, int i) {
-		sumArr[i] = (sumArr[i - 1] + Integer.parseInt(input[i - 1])) % M;
-	}
-
-	private static int combination(int n, int r) {
-
-		if (n == r || r == 0) {
-			return 1;
-		}
-
-		return combination(n - 1, r - 1) + combination(n - 1, r);
 	}
 }
--- solved/gold/BOJ1253.java
+++ solved/gold/BOJ1253.java
@@ -1,41 +1,79 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class BOJ1253 {
-
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) throws IOException {
-
 		int N = Integer.parseInt(br.readLine());
-		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		Arrays.sort(input);
+		int[] inputNums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		/**
+		 * 투 포인터를 위해서 정렬
+		 */
+		mergeSort(inputNums);
 		int count = 0;
 
 		for (int i = 0; i < N; i++) {
-			int targetNum = input[i];
+			int targetNum = inputNums[i];
 			int targetIdx = i;
 			int startIdx = 0;
 			int endIdx = N - 1;
 
+			/**
+			 * 처음과 끝에서 시작해서 targetNum을 찾는다.
+			 */
 			while (startIdx < endIdx) {
-				if (startIdx != targetIdx && endIdx != targetIdx) {
-					int start = input[startIdx];
-					int end = input[endIdx];
-					long sum = start + end;
+				if (startIdx == targetIdx) startIdx++;
+				else if (endIdx == targetIdx) endIdx--;
+				else {
+					int startNum = inputNums[startIdx];
+					int endNum = inputNums[endIdx];
+					long sum = startNum + endNum;
 
-					if (sum == targetNum) {
+					if (sum < targetNum) startIdx++;
+					else if (sum > targetNum) endIdx--;
+					else {
 						count++;
+//					System.out.println(startNum + " +  " + endNum + " = " + sum);
 						break;
-					} else if (sum < targetNum) startIdx++;
-					else endIdx--;
-				} else if (startIdx == targetIdx) startIdx++;
-				else endIdx--;
+					}
+				}
 			}
 		}
 		bw.write(count + "\n");
 		bw.flush();
 		bw.close();
+	}
+
+	static void mergeSort(int[] inputNums) {
+		if (inputNums == null || inputNums.length <= 1) return;
+
+		int mid = inputNums.length / 2;
+		int[] leftArr = Arrays.copyOfRange(inputNums, 0, mid);
+		int[] rightArr = Arrays.copyOfRange(inputNums, mid, inputNums.length);
+
+		mergeSort(leftArr);
+		mergeSort(rightArr);
+
+		merge(inputNums, leftArr, rightArr);
+	}
+
+	static void merge(int[] inputNums, int[] leftArr, int[] rightArr) {
+		int i = 0, l = 0, r = 0;
+
+		while (l < leftArr.length && r < rightArr.length) {
+			if (leftArr[l] <= rightArr[r]) inputNums[i++] = leftArr[l++];
+			else inputNums[i++] = rightArr[r++];
+		}
+
+		while (l < leftArr.length) {
+			inputNums[i++] = leftArr[l++];
+		}
+
+		while (r < rightArr.length) {
+			inputNums[i++] = rightArr[r++];
+		}
 	}
 }
