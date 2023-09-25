e5b7ac9a019a3adb4a31023a58864c462d83773e
yback1223 <yoyob1223@gmail.com>
2023-09-25 20:30:30 +0900

    Solve 1 platinum - 2 time

--- solved/platinum/BOJ1517.java
+++ solved/platinum/BOJ1517.java
@@ -1,88 +1,58 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class BOJ1517 {
 
-	static long swaps;
+	static long sum = 0L;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		long[] inputArr = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
-		Comp[] arr = new Comp[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = new Comp(i, inputArr[i]);
-		}
-//		System.out.println("===Before Sort===");
-//		printComp(N, arr);
-
-		mergeSort(arr);
-
-//		System.out.println("===After Sort===");
-//		printComp(N, arr);
-
-		bw.write(swaps + "\n");
+		int[] inputNums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//		System.out.println(Arrays.toString(inputNums));
+		mergeSort(inputNums);
+//		System.out.println(Arrays.toString(inputNums));
+		bw.write(sum + "\n");
 		bw.flush();
 		bw.close();
 	}
 
-	private static void printComp(int N, Comp[] arr) {
-		for (int i = 0; i < N; i++) {
-			System.out.print(arr[i].val + " ");
+	static void mergeSort(int[] inputNums) {
+		if (inputNums == null || inputNums.length <= 1) {
+			return;
 		}
-		System.out.println();
 
-		for (int i = 0; i < N; i++) {
-			System.out.print(arr[i].idx + " ");
-		}
-		System.out.println();
+		int mid = inputNums.length / 2;
+		int[] leftArr = Arrays.copyOfRange(inputNums, 0, mid);
+		int[] rightArr = Arrays.copyOfRange(inputNums, mid, inputNums.length);
+
+		mergeSort(leftArr);
+		mergeSort(rightArr);
+
+		merge(inputNums, leftArr, rightArr);
 	}
 
-	static class Comp implements Comparable<Comp> {
-		int idx;
-		long val;
+	static void merge(int[] inputNums, int[] leftArr, int[] rightArr) {
+		int i = 0, l = 0, r = 0;
 
-		Comp(int idx, long val) {
-			this.idx = idx;
-			this.val = val;
-		}
-
-		@Override
-		public int compareTo(Comp o) {
-			return Long.compare(this.val, o.val);
-		}
-	}
-
-	private static long mergeSort(Comp[] arr) {
-		if (arr == null || arr.length <= 1) {
-			return 0;
-		}
-
-		int mid = arr.length / 2;
-		Comp[] left = Arrays.copyOfRange(arr, 0, mid);
-		Comp[] right = Arrays.copyOfRange(arr, mid, arr.length);
-
-		swaps = mergeSort(left) + mergeSort(right);
-
-		int i = 0, j = 0, k = 0;
-
-		while (i < left.length && j < right.length) {
-			if (left[i].val <= right[j].val) {
-				arr[k++] = left[i++];
+		while (l < leftArr.length && r < rightArr.length) {
+			if (leftArr[l] <= rightArr[r]) {
+				inputNums[i++] = leftArr[l++];
 			} else {
-				arr[k++] = right[j++];
-				swaps += left.length - i;
+				inputNums[i++] = rightArr[r++];
+				sum += leftArr.length - l;
 			}
-		}
-		while (i < left.length) {
-			arr[k++] = left[i++];
-		}
-		while (j < right.length) {
-			arr[k++] = right[j++];
+//			System.out.println(sum);
 		}
 
-		return swaps;
+		while (l < leftArr.length) {
+			inputNums[i++] = leftArr[l++];
+		}
+
+		while (r < rightArr.length) {
+			inputNums[i++] = rightArr[r++];
+		}
 	}
 }
