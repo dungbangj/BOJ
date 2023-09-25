dc1ab99796dca96020af7c27627e32da5d36d855
yback1223 <yoyob1223@gmail.com>
2023-09-25 19:55:30 +0900

    Solve 1 bronze - 2 time

--- solved/bronze/BOJ10989.java
+++ solved/bronze/BOJ10989.java
@@ -1,21 +1,29 @@
 import java.io.*;
-import java.util.*;
 
 public class BOJ10989 {
+
+	static final int MAX = 10_000;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+		int[] counts = new int[MAX + 1];
+		int maxVal = Integer.MIN_VALUE;
 
-		for (int i = 0; i < N; ++i) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(arr);
 		for (int i = 0; i < N; i++) {
-			bw.write(arr[i] + "\n");
+			int inputNum = Integer.parseInt(br.readLine());
+			counts[inputNum]++;
+			if (inputNum > maxVal) maxVal = inputNum;
 		}
+
+		for (int i = 0; i <= maxVal; i++) {
+			while (counts[i] != 0) {
+				bw.write(i + "\n");
+				counts[i]--;
+			}
+		}
+
 		bw.flush();
 		bw.close();
 	}
